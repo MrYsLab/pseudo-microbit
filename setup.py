e34a5cac36d2b3a2930847aa9029a12bcd0772e4
MrYsLab <MrYsLab@gmail.com>
2018-01-31 11:30:58 -0500

    Version 1.8

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from setuptools import setup
 
 setup(
     name='pseudo-microbit',
-    version='1.7',
+    version='1.8',
     packages=['microbit'],
     py_modules=['microbit', 'compass', 'display',
                 'i2c', 'spi', 'uart', 'music', 'neopixel',
                 'os', 'speech', 'radio'],
     install_requires=['microfs',
-                      'uflash'],
+                      'uflash',
+                      'pyminifier'],
     url='https://github.com/MrYsLab/pseudo-microbit',
     download_url='https://github.com/MrYsLab/pseudo-microbit',
     license='MIT',
